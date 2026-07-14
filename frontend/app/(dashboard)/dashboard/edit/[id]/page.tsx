eb3004318004f020d6222af1bfea87bc5df4ea60
kafura0 <kabura.n.joan@gmail.com>
2026-07-14 16:10:33 +0300

    Fix unused wd variable

--- frontend/app/(dashboard)/dashboard/edit/[id]/page.tsx
+++ frontend/app/(dashboard)/dashboard/edit/[id]/page.tsx
@@ -25,6 +25,7 @@ export default function DashboardEditPage() {
         const res = await dashboardsApi.getData(id);
         if (cancelled) return;
         const d = res.data.dashboard;
+        const widgetData = res.data.widget_data;
         setWidgets(d.widgets as WidgetConfig[]);
         setName(d.name);
 
@@ -88,7 +89,7 @@ export default function DashboardEditPage() {
           widgets={widgets}
           columns={columns}
           onChange={setWidgets}
-          widgetData={wd}
+          widgetData={widgetData}
         />
       </div>
     </div>
